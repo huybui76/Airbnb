1af076a13081b07ed5ec1f53d6aa31cc599a9107
Bùi Quang Huy <59251361+huybui76@users.noreply.github.com>
2023-11-22 21:47:23 +0700

    Set up router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as SecureStore from "expo-secure-store";
+
+const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -19,9 +40,12 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const router = useRouter()
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    'mon': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'mon-b': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf')
+
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -43,14 +67,38 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+  const router = useRouter()
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name='(modals)/login' options={{
+        presentation: 'modal',
+        headerTitleStyle: {
+          fontFamily: 'mon-sb',
+
+        },
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <AntDesign name="close" size={24} color="black" />
+          </TouchableOpacity>
+        )
+
+      }} />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: '' }} />
+      <Stack.Screen name="(modals)/booking" options={{
+        headerTitle: '',
+        presentation: 'transparentModal',
+        animation: 'fade',
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => router.back()}>
+            <AntDesign name="close" size={24} color="black" />
+          </TouchableOpacity>
+        )
+      }} />
+    </Stack>
+
+
+
   );
 }
